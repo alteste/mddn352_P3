934d093d6b0c33503b1f020a675c62900d0c8b86
alteste <jb.campbell13@hotmail.com>
2017-05-24 18:49:31 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-                $("#logo").click(function(){
+                $("#loginButton").click(function(){
                     $('.login').hide(1);
                     $('.forecast').show(1);
                 });
