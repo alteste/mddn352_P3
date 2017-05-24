1c46deb5e0265e2b0e47e802859c8335d719dde0
alteste <jb.campbell13@hotmail.com>
2017-05-24 18:42:29 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+                $("#logo").click(function(){
+                    $('.login').hide(1);
+                    $('.forecast').show(1);
+                });
+            });
